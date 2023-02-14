7d005753f3f8f3145734580e09fa096feb05db71
Jenny Carroll <jennygc@telus.net>
2023-02-14 20:53:45 +0000

    I refactored my function to have a == in the if statement rather than === and added a return.  I commented out the extra settimeout

--- typewriter.js
+++ typewriter.js
@@ -2,15 +2,15 @@ const sentence = "hello there from lighthouse labs";
 let time = 0;
 for (const char in sentence) {
   setTimeout(() => {
-    // console.log("test", sentence.length - 1, char);
-    if (sentence.length - 1 === char) {
+    if (sentence.length - 1 == char) {
       process.stdout.write(`${sentence[char]} \n`);
+      return;
     }
     process.stdout.write(sentence[char]);
   }, time);
   time += 50;
 }
-
-setTimeout(() => {
-  process.stdout.write("\n");
-}, time + 1);
+//or I could take out the if statement and do the below
+// setTimeout(() => {
+//   process.stdout.write("\n");
+// }, time + 1);
